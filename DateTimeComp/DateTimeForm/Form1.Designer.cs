d2594397a32f87db6df61704674d6fba0f9e7c60
Mert Burak Dervişoğlu <mertburakdervisoglu@gmail.com>
2022-09-08 05:34:24 +0300

    ikinci push gün ve hafta durumları
    Günlüğe göre haftalığa göre doldurulacak datalar ayarlandı
    Başlangıç tarihine göre gün yada hafta seçimiyle tablo seçildi olarak görüntüleniyor
    Listbox içerisine seçimi yapılan tarihler ekleniyor
    haftalık seçime tıklandığında gün butonları aktifleşiyor ve başlangıç tarihine göre ileriye doğru kaç hafta seçimi yapıldıysa buna göre listbox içerisine o tarihler ekleniyor

--- DateTimeComp/DateTimeForm/Form1.Designer.cs
+++ DateTimeComp/DateTimeForm/Form1.Designer.cs
@@ -33,6 +33,13 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pazartesi = new System.Windows.Forms.Button();
+            this.sali = new System.Windows.Forms.Button();
+            this.carsamba = new System.Windows.Forms.Button();
+            this.persembe = new System.Windows.Forms.Button();
+            this.cuma = new System.Windows.Forms.Button();
+            this.cumartesi = new System.Windows.Forms.Button();
+            this.pazar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // monthCalendar1
@@ -47,6 +54,7 @@
             this.monthCalendar2.Location = new System.Drawing.Point(654, 238);
             this.monthCalendar2.Name = "monthCalendar2";
             this.monthCalendar2.TabIndex = 2;
+            this.monthCalendar2.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar2_DateChanged);
             // 
             // listBox1
             // 
@@ -75,11 +83,90 @@
             this.comboBox2.TabIndex = 5;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
+            // pazartesi
+            // 
+            this.pazartesi.Location = new System.Drawing.Point(29, 165);
+            this.pazartesi.Name = "pazartesi";
+            this.pazartesi.Size = new System.Drawing.Size(75, 23);
+            this.pazartesi.TabIndex = 6;
+            this.pazartesi.Text = "Pazartesi";
+            this.pazartesi.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.pazartesi.UseVisualStyleBackColor = true;
+            this.pazartesi.Click += new System.EventHandler(this.pazartesi_Click);
+            // 
+            // sali
+            // 
+            this.sali.Location = new System.Drawing.Point(165, 165);
+            this.sali.Name = "sali";
+            this.sali.Size = new System.Drawing.Size(75, 23);
+            this.sali.TabIndex = 7;
+            this.sali.Text = "Salı";
+            this.sali.UseVisualStyleBackColor = true;
+            this.sali.Click += new System.EventHandler(this.sali_Click);
+            // 
+            // carsamba
+            // 
+            this.carsamba.Location = new System.Drawing.Point(314, 164);
+            this.carsamba.Name = "carsamba";
+            this.carsamba.Size = new System.Drawing.Size(75, 23);
+            this.carsamba.TabIndex = 8;
+            this.carsamba.Text = "Çarşamba";
+            this.carsamba.UseVisualStyleBackColor = true;
+            this.carsamba.Click += new System.EventHandler(this.carsamba_Click);
+            // 
+            // persembe
+            // 
+            this.persembe.Location = new System.Drawing.Point(29, 226);
+            this.persembe.Name = "persembe";
+            this.persembe.Size = new System.Drawing.Size(75, 23);
+            this.persembe.TabIndex = 9;
+            this.persembe.Text = "Perşembe";
+            this.persembe.UseVisualStyleBackColor = true;
+            this.persembe.Click += new System.EventHandler(this.persembe_Click);
+            // 
+            // cuma
+            // 
+            this.cuma.Location = new System.Drawing.Point(165, 226);
+            this.cuma.Name = "cuma";
+            this.cuma.Size = new System.Drawing.Size(75, 23);
+            this.cuma.TabIndex = 10;
+            this.cuma.Text = "Cuma";
+            this.cuma.UseVisualStyleBackColor = true;
+            this.cuma.Click += new System.EventHandler(this.cuma_Click);
+            // 
+            // cumartesi
+            // 
+            this.cumartesi.Location = new System.Drawing.Point(314, 226);
+            this.cumartesi.Name = "cumartesi";
+            this.cumartesi.Size = new System.Drawing.Size(75, 23);
+            this.cumartesi.TabIndex = 11;
+            this.cumartesi.Text = "Cumartesi";
+            this.cumartesi.UseVisualStyleBackColor = true;
+            this.cumartesi.Click += new System.EventHandler(this.cumartesi_Click);
+            // 
+            // pazar
+            // 
+            this.pazar.Location = new System.Drawing.Point(24, 284);
+            this.pazar.Name = "pazar";
+            this.pazar.Size = new System.Drawing.Size(75, 23);
+            this.pazar.TabIndex = 12;
+            this.pazar.Text = "Pazar";
+            this.pazar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.pazar.UseVisualStyleBackColor = true;
+            this.pazar.Click += new System.EventHandler(this.pazar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1281, 536);
+            this.Controls.Add(this.pazar);
+            this.Controls.Add(this.cumartesi);
+            this.Controls.Add(this.cuma);
+            this.Controls.Add(this.persembe);
+            this.Controls.Add(this.carsamba);
+            this.Controls.Add(this.sali);
+            this.Controls.Add(this.pazartesi);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listBox1);
@@ -99,5 +186,12 @@
         private ListBox listBox1;
         private ComboBox comboBox1;
         private ComboBox comboBox2;
+        private Button pazartesi;
+        private Button sali;
+        private Button carsamba;
+        private Button persembe;
+        private Button cuma;
+        private Button cumartesi;
+        private Button pazar;
     }
 }
--- DateTimeComp/DateTimeForm/Form1.cs
+++ DateTimeComp/DateTimeForm/Form1.cs
@@ -1,4 +1,5 @@
 using DateTimeForm.Entities;
+using DateTimeForm.Entities.Selected;
 
 namespace DateTimeForm
 {
@@ -6,34 +7,97 @@ namespace DateTimeForm
     {
         List<string> strings = new List<string> { "Günlük", "Haftalýk" };
         List<WeekOrDay> weekOrDayList = new List<WeekOrDay>();
+        SelectionRange sr = new SelectionRange();
+        List<DayOfWeek> selectedDays = new List<DayOfWeek>();
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-            
-        }
-
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            comboBox2.Items.Clear();
             ComboBox comboBox = (ComboBox)sender;
             string selectedEmployee = (string)comboBox1.SelectedItem;
             int z = 1;
-            var days=weekOrDayList.FirstOrDefault(x => x.Value==selectedEmployee);
+            var days = weekOrDayList.FirstOrDefault(x => x.Value == selectedEmployee);
             foreach (var day in days.timeline)
             {
                 comboBox2.Items.Add(day.Id + ". " + day.Value);
             }
+            if (selectedEmployee == strings[1])
+            {
+                pazartesi.Visible = true;
+                sali.Visible = true;
+                carsamba.Visible = true;
+                persembe.Visible = true;
+                cuma.Visible = true;
+                cumartesi.Visible = true;
+                pazar.Visible = true;
+            }
+            if (selectedEmployee == strings[0])
+            {
+                pazartesi.Visible = false;
+                sali.Visible = false;
+                carsamba.Visible = false;
+                persembe.Visible = false;
+                cuma.Visible = false;
+                cumartesi.Visible = false;
+                pazar.Visible = false;
+            }
         }
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
+            sr.Start = monthCalendar1.SelectionRange.Start;
+         
+            ComboBox comboBox = (ComboBox)sender;
+            string selected1 = (string)comboBox1.SelectedItem;
+            string selected2 = (string)comboBox2.SelectedItem;
+            var charint = selected2.Split('.');
+            var days = weekOrDayList.FirstOrDefault(x => x.Value == selected2);
+            int selectedDateRange = Convert.ToInt16(charint[0]);
+            if (selected1 == strings[0])
+            {
+                listBox1.Items.Clear();
+                sr.End = sr.Start.Date.AddDays(selectedDateRange - 1);
+                var listValues = sr.End - sr.Start;
+                List<string> tarihListesi = new List<string>();
+                for (DateTime tarih = sr.Start; tarih <= sr.End; tarih = tarih.AddDays(1))
+                {
+                    listBox1.Items.Add(tarih.ToShortDateString());
+                    tarihListesi.Add(tarih.ToShortDateString());
+                }
+                sr.Start = DateTime.Parse(tarihListesi[0]);
+                sr.End = DateTime.Parse(tarihListesi.Skip(tarihListesi.Count() - 1).First());
+                monthCalendar2.SelectionRange = sr;
+            }
+            else if (selected1 == strings[1] && selectedDays.Count<1)
+            {
+                listBox1.Items.Clear();
+                sr.End = sr.Start.Date.AddDays((selectedDateRange * 7) - 1);
+                var listValues = sr.End - sr.Start;
+                List<string> tarihListesi = new List<string>();
+                for (DateTime tarih = sr.Start; tarih <= sr.End; tarih = tarih.AddDays(1))
+                {
+                    listBox1.Items.Add(tarih.ToShortDateString());
+                    tarihListesi.Add(tarih.ToShortDateString());
+                }
+                sr.Start = DateTime.Parse(tarihListesi[0]);
+                sr.End = DateTime.Parse(tarihListesi.Skip(tarihListesi.Count() - 1).First());
+                monthCalendar2.SelectionRange = sr;
+            }
 
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            sr.Start = DateTime.Now;
+            pazartesi.Visible = false;
+            sali.Visible = false;
+            carsamba.Visible = false;
+            persembe.Visible = false;
+            cuma.Visible = false;
+            cumartesi.Visible = false;
+            pazar.Visible = false;
             int z = 1;
             foreach (string s in strings)
             {
@@ -57,7 +121,87 @@ namespace DateTimeForm
 
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
         {
-            
+            SelectionRange sr = new SelectionRange();
+            sr.Start = monthCalendar2.SelectionRange.Start.Date;
+        }
+
+        private void monthCalendar2_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            SelectionRange sr = new SelectionRange();
+            sr.End = monthCalendar2.SelectionRange.End.Date;
+        }
+
+        private void pazartesi_Click(object sender, EventArgs e)
+        {
+            DateSelector(pazartesi,DayOfWeek.Monday);
+        }
+
+        private void sali_Click(object sender, EventArgs e)
+        {
+            DateSelector(sali, DayOfWeek.Tuesday);
+           
+        }
+
+        private void carsamba_Click(object sender, EventArgs e)
+        {
+            DateSelector(carsamba, DayOfWeek.Wednesday);
+        }
+
+        private void persembe_Click(object sender, EventArgs e)
+        {
+            DateSelector(persembe, DayOfWeek.Thursday);
+        }
+
+        private void cuma_Click(object sender, EventArgs e)
+        {
+            DateSelector(cuma, DayOfWeek.Friday);
+        }
+
+        private void cumartesi_Click(object sender, EventArgs e)
+        {
+            DateSelector(cumartesi, DayOfWeek.Saturday);
+        }
+
+        private void pazar_Click(object sender, EventArgs e)
+        {
+            DateSelector(cumartesi, DayOfWeek.Sunday);
+        }
+        public void DateSelector(Button button,DayOfWeek dayOfWeek)
+        {
+            sr.Start = monthCalendar1.SelectionRange.Start.Date;
+            listBox1.Items.Clear();
+            string selected1 = (string)comboBox1.SelectedItem;
+            string selected2 = (string)comboBox2.SelectedItem;
+            if (selected1 == strings[1] && selected2 != null)
+            {
+                if (button.BackColor != System.Drawing.Color.Red)
+                {
+                    button.BackColor = System.Drawing.Color.Red;
+                    selectedDays.Add(dayOfWeek);
+                }
+                else
+                {
+                    button.BackColor = default;
+                    selectedDays.Remove(dayOfWeek);
+                }
+                var charint = selected2.Split('.');
+                var days = weekOrDayList.FirstOrDefault(x => x.Value == selected2);
+                int selectedDateRange = Convert.ToInt16(charint[0]);
+                sr.End = sr.Start.Date.AddDays((selectedDateRange * 7) - 1);
+                var listValues = sr.End - sr.Start;
+                List<string> tarihListesi = new List<string>();
+                List<DateTime> tarihListesidate = new List<DateTime>();
+                for (DateTime tarih = sr.Start; tarih <= sr.End; tarih = tarih.AddDays(1))
+                {
+                    tarihListesidate.Add(tarih);
+                    if (selectedDays.Contains(tarih.DayOfWeek))
+                    {
+                        listBox1.Items.Add(tarih.ToShortDateString() + " => " + tarih.DayOfWeek);
+                    }
+
+                    tarihListesi.Add(tarih.ToShortDateString());
+                }
+            }
         }
     }
 }
